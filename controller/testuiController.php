b19199910dcfa358737b5059fd54382601de8426
bhanu007 <bhanu.gill@osscube.com>
2013-05-08 06:02:38 -0700

    test ui has been changed

--- controller/testuiController.php
+++ controller/testuiController.php
@@ -3,16 +3,23 @@ class testuiController extends common{
 	function abc() {
 		echo "SDS";//$this->loadView("main_page");
 	}
- function category() {
- 	$this->loadView("header");
- 	$this->loadView("user_examiner_view/deshboard_menu");
- 	$this->loadView("user_examiner_view/category");
- }
  function bhanu() {
  	$this->loadView("header");
  	$this->loadView("user_header");
  	$this->loadView("user_examiner_view/deshboard_menu");
 
+ }
+ function category() {
+ 	$this->loadView("header");
+ 	$this->loadView("user_header");
+ 	$this->loadView("user_examiner_view/deshboard_menu");
+ 	$this->loadView("user_examiner_view/category");
+ }
+ function mytest(){
+    $this->loadView("header");
+ 	$this->loadView("user_header");
+ 	$this->loadView("user_examiner_view/deshboard_menu");
+    $this->loadView("user_examiner_view/maketestpage");
  }
  function suraj() {
  	$this->loadView("header");
