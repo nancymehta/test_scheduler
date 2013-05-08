78bd8a7e6af86ef786095905eaf521d7c8634ea6
gouravkhanna <gourav.khanna@osscube.com>
2013-05-08 00:36:17 -0700

    added knowledge base and completed user Controller

--- controller/userController.php
+++ controller/userController.php
@@ -8,5 +8,44 @@ class userController extends mainController {
 		$this->loadView("user_examiner_view/deshboard_menu");
 		$this->loadView("user_examiner_view/maketestpage");
 	}
+	/*provide view for category tab*/
+	function category() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	 	$this->loadView("user_examiner_view/category");
+	}
+	/*provide view for category tab*/
+	function mytest() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	 	$this->loadView("user_examiner_view/maketestpage");
+	}
+	/*provide view for category tab*/
+	function questionbank() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	}
+	/*provide view for category tab*/
+	function certificate() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	}
+	/*provide view for result tab*/
+	function result() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	}
+	/*provide view for assign tab*/
+	function assign() {
+		$this->loadView("header");
+		$this->loadView("user_header");
+	 	$this->loadView("user_examiner_view/deshboard_menu");
+	}
+
 }
 ?>
--- view/user_examiner_view/deshboard_menu.php
+++ view/user_examiner_view/deshboard_menu.php
@@ -1,19 +1,19 @@
 
 <div class = "menu_list">
 	<ul>
-		<li><a id = "home" href = "#">Home</a></li>
+		<li><a id = "home" href ="<?php echo SITE_PATH; ?>">Home</a></li>
 		<li><a id = "assign" href = "assign">Assign</a></li>
 		<li><a id = "test" href = "#">Test</a></li>
-		<li><a id = "result" href = "#">Results</a></li>
+		<li><a id = "result" href = "<?php echo SITE_PATH."result"; ?>">Results</a></li>
 	</ul>
 </div>
 <div class = "sub-menu_list">
 	<div class = "test-sub-menu">
 		<ul>
-			<li><a href ="maketest">My Tests</a></li>
-			<li><a href = "#">Question Bank</a></li>
-			<li><a href = "#">categories</a></li>
-			<li><a href = "#">certificates</a></li>
+			<li><a href = "<?php echo SITE_PATH."mytest"; ?>">My Tests</a></li>
+			<li><a href = "<?php echo SITE_PATH."questionbank"; ?>">Question Bank</a></li>
+			<li><a href = "<?php echo SITE_PATH."category"; ?>">categories</a></li>
+			<li><a href = "<?php echo SITE_PATH."certificate"; ?>">certificates</a></li>
 		</ul>	
 	</div>
 </div>
