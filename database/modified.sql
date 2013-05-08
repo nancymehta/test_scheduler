f67f8fafec662db66fc060af7eb5608b96b93910
shashank13verma <shashank.verma@osscube.com>
2013-05-08 08:15:36 -0700

    moddified db with more dummy data

--- database/modified.sql
+++ database/modified.sql
@@ -77,6 +77,8 @@ CHANGE `feedback` `feedback` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_gene
 ALTER TABLE `question` CHANGE `question` `question` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'question asked by the person';
 ALTER TABLE `question_options` CHANGE `correct` `correct` ENUM( '0', '1' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '''0'' for incorrect and ''1'' for correct';
 
+/*Inserting dummy data into various tables */
+
 INSERT INTO `category` ( `id` , `name` , `created_on` , `updated_on` , `created_by` , `updated_by` )
 VALUES (
 NULL , 'PHP',
@@ -101,3 +103,75 @@ NULL , '1', 'PHP page', '0',
 CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '1', '1', '0'
 );
 
+INSERT INTO `user_profile` ( `id` , `user_id` , `first_name` , `last_name` , `email` , `type_of_org_id` , `created_on` , `updated_on` , `created_by` , `updated_by` , `ip_address` )
+VALUES (
+NULL , '1', 'Parveen', 'Badoni', 'parveen.badoni@osscube.com', '4',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '1', NULL , NULL
+);
+
+INSERT INTO `user_profile` ( `id` , `user_id` , `first_name` , `last_name` , `email` , `type_of_org_id` , `created_on` , `updated_on` , `created_by` , `updated_by` , `ip_address` )
+VALUES (
+NULL , '2', 'Jasleen', 'Kaur', 'jasleen.kaur@osscube.com', '4',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL , NULL
+);
+
+INSERT INTO `category` ( `id` , `name` , `created_on` , `updated_on` , `created_by` , `updated_by` )
+VALUES (
+NULL , 'MySql',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL
+);
+
+INSERT INTO `test` ( `id` , `name` , `total_ques` , `created_on` , `updated_on` , `created_by` , `updated_by` , `status` )
+VALUES (
+NULL , 'Test1', '10',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL , '0'
+);
+
+INSERT INTO `test_category` ( `id` , `test_id` , `cat_id` , `no_of_ques` , `created_on` , `updated_on` , `created_by` , `updated_by` )
+VALUES (
+NULL , '1', '1', '6',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL
+), (
+NULL , '1', '2', '4',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL
+);
+
+INSERT INTO `master` ( `id` , `code_type` , `code_value` )
+VALUES (
+NULL , 'certificate', 'MySql certificate'
+), (
+NULL , 'certificate', 'certificate for 1st ranker'
+);
+
+/*altering certificate table*/
+ALTER TABLE `certificate` ADD `certificate_id` INT NOT NULL AFTER `id` ;
+
+/*altering test_link table*/
+
+ALTER TABLE `test_link` CHANGE `no_of_attempts` `no_of_attempts` TINYINT( 3 ) NULL ,
+CHANGE `certificate_id` `certificate_id` INT( 11 ) NULL ;
+
+ALTER TABLE test_link DROP FOREIGN KEY `test_link_ibfk_3` ;
+
+ALTER TABLE test_link ADD FOREIGN KEY ( `certificate_id` ) REFERENCES `master` ( `id` ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+
+INSERT INTO `test_link` ( `id` , `test_id` , `start_time` , `end_time` , `link` , `random` , `per_page_ques` , `feedback` , `score` , `email_results` , `collect_firstname` , `collect_lastname` , `collect_email_enroll` , `test_avail_opt1` , `test_avail_opt2` , `display_result_id` , `certificate` , `no_of_attempts` , `certificate_id` , `time_limit` , `select_answer` , `pass_marks` , `created_on` , `updated_on` , `created_by` , `updated_by` , `status` )
+VALUES (
+NULL , '1', '2013-05-08 20:20:02', '2013-05-08 22:08:10', 'gikjhkjkjlklllhhfddsfgjukjk', '1', '8', '0', '1', '0', '1', '1', '1', '0', '0', '1', '1', '2', '19', '60', '1', '50',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL , '0'
+);
+
+INSERT INTO `test_taker` ( `id` , `test_link_id` , `email_enroll_no` , `start_time` , `end_time` , `test_id` , `ques_attempted` , `score` , `created_on` , `updated_on` , `created_by` , `updated_by` )
+VALUES (
+NULL , '1', '1111', '2013-05-08 20:10:31', '2013-05-08 21:10:36', '1', '5', '30',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL
+);
+
+ALTER TABLE `test_taker_ques` CHANGE `answer_given` `answer_given` INT NULL ;
+ALTER TABLE test_taker_ques ADD FOREIGN KEY ( `answer_given` ) REFERENCES `question_options` ( `id` ) ON DELETE CASCADE ON UPDATE CASCADE ;
+
+INSERT INTO `test_taker_ques` ( `id` , `test_taker_id` , `ques_id` , `answer_given` , `created_on` , `updated_on` , `created_by` , `updated_by` , `feedback` )
+VALUES (
+NULL , '1', '1', '1',
+CURRENT_TIMESTAMP , '0000-00-00 00:00:00', '2', NULL , NULL
+);
