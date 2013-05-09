bd39ff6986ec808c564b3bcbfd632a86cab53d5c
rawatsun <suraj.singh@osscube.com>
2013-05-09 03:05:21 -0700

    css edited

--- view/user_header.php
+++ view/user_header.php
@@ -6,6 +6,8 @@
 				<font style="color:black">test</font>scheduler</h1><br>
 			<span class="fontsize24 colorwhite" ><h2><?php echo "WELCOME".@$_SESSION['SESS_USER_NAME'];?></h2></span>	<br><br>
 <a class="floatr" href="<?php echo SITE_PATH."logout"; ?>"> LOGOUT</a>
+
+<a class="floatr" href="<?php echo SITE_PATH."logout"; ?>"> Account Settings</a>
 			</div>
 				
 </div>
