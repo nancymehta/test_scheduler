d0ca393e12b106677a138ecb82e0fb43bed1321b
rawatsun <suraj.singh@osscube.com>
2013-05-09 04:18:18 -0700

    generic js added

--- view/user_header.php
+++ view/user_header.php
@@ -1,6 +1,6 @@
 <div class = "mainContainer">
 		
-			<div class="header">
+			<div class="header bg-vib-blue">
 				<div class="logo"><img src="<?php echo IMAGE_PATH;?>/logo.png"></div>
 				<h1>
 				<font style="color:black">test</font>scheduler</h1><br>
