27a6117388e8700f1df9a773af30b92fbfe2b002
gouravkhanna <gourav.khanna@osscube.com>
2013-05-09 08:01:30 -0700

    Admin controller and admin login done

---
+++ view/admin_view/admin_deshboard_menu.php
@@ -0,0 +1,18 @@
+<div class="menus">
+<div class = "menu_list">
+	<ul>
+		<li><a id = "home" href ="<?php echo SITE_PATH; ?>">Home</a></li>
+		<li><a id = "usermanagement" 
+			href = '<?php echo SITE_PATH."usermanagement";?>'>
+			User Management</a></li>
+		<li><a id = "testmanagement" 
+			href = '<?php echo SITE_PATH."testmanagement";?>'
+			>Test Management</a></li>
+		<li><a id = "feedbackmanagement" 
+			href = '<?php echo SITE_PATH."feedbackmanagement";?>'>
+			feedback</a></li>
+
+	</ul>
+</div>
+
+</div>
