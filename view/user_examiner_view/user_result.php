69f99917dcbec87a7f0dbf11081a206fac0b4259
bhanu007 <bhanu.gill@osscube.com>
2013-05-08 23:32:26 -0700

    files added

---
+++ view/user_examiner_view/user_result.php
@@ -0,0 +1,33 @@
+<div class="user-result">
+
+	<span class="">Name of Exam Taker : <label class="" id="user-result-name" name="user-result-name">Sourab</label></span> 
+	<br>
+	<br>
+	<span class="">Ip Address : </span> <label class="" id="user-result-ip" name="user-result-ip">192.168.1.1</label>  
+	<br>
+	<span class="">Id  Assigned  : </span> <label class="" id="user-result-id" name="user-result-id">12345</label>  
+	<hr>
+	<span class="">Score   : </span> <label class="" id="user-result-score" name="user-result-score">90</label>  <br>
+	<span class="">Percentage :</span> <label class="" id="user-result-per" name="user-result-per">50%</label>  <br>
+	<span class="">Duration :</span> <label class="" id="user-result-duration" name="user-result-duration">30 hrs</label>  <br>
+	<span class="">Date Started : </span> <label class="" id="user-result-date" name="user-result-date">1-3-4</label>  <br>
+
+	<span class="">Date Finished : </span> <label class="" id="user-result-dateend" name="user-result-dateend">1-2-3</label>  <br>
+	<hr>
+	<span class="">Email : </span> <label class="" id="user-result-mail" name="user-result-mail">sourab@gmail.com</label>  <br>
+	<br>
+	<div class="worng-right-questions" style="text-align:center; height:20px; width:100%; background-color:#eee">
+
+	10 out of 75 
+
+
+</div>
+<br>
+	<div class="questionanswers">
+		Q1. blah blah ... 
+		 
+	</div>
+
+
+
+</div>
---
+++ view/user_examiner_view/users_overall_result.php
@@ -0,0 +1,52 @@
+<div class="user_overall_result">
+
+<div class="search_test">
+<span class="">Select Test </span> <div class="search_test" name="search_test" id="search_test">
+	<select>
+		<option>1</option>
+
+		<option>2</option>
+
+		<option>3</option>
+
+	</select>
+</div>
+
+</div>
+
+
+<div class="all_result_table">
+<div class="overall" style="text-align:center">
+
+
+</div>
+<table>
+<tr>
+	<th>OverAll Result</th>
+	<th>Pecentage</th>
+	<th>Score</th>
+	<th>Duration</th>
+	<th>Attempt</th>
+</tr>
+<tr><td></td><td>1</td><td>1</td><td>1</td><td>1</td></tr>
+</table>
+<br>
+<table>
+<tr>
+	<th>Name 		</th>
+	<th>Pecentage</th>
+	<th>Score</th>
+	<th>Duration</th>
+
+	<th>View Result </th>
+</tr>
+<tr><td>1</td><td>1</td><td>1</td><td>1</td><td><a href="">click  to View</a></td></tr>
+</table>
+
+
+
+
+
+</div>
+
+</div>
