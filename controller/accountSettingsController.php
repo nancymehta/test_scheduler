3662b327b73f68823b5990c9f47ca8bd1c4a7d7d
shashank13verma <shashank.verma@osscube.com>
2013-05-09 02:58:08 -0700

    account settings controller file

---
+++ controller/accountSettingsController.php
@@ -0,0 +1,12 @@
+<?php
+class accountSettingsController extends common{
+	
+	
+	
+	function accountSettings() {
+	try {
+   		
+	} catch (Exception $e) {
+	  	$this->handleException($e->getMessage());
+	} 		
+	}
---
+++ model/accountSettingsModel.php
@@ -0,0 +1,8 @@
+<?php 
+include MODEL_PATH."db_connect.php";
+class accountSettingsModel extends dbConnectModel {
+	function __construct() {
+		parent::__construct();
+	}
+	
+}
