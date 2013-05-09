48a61309ee62106e7514b47024bfcd3b709cbc05
shashank13verma <shashank.verma@osscube.com>
2013-05-08 21:33:15 -0700

    question bank model

--- controller/questionBankController.php
+++ controller/questionBankController.php
@@ -1,7 +1,16 @@
 <?php
 include SITE_ROOT.'controller/mainController.php';
 class questionBankController extends mainController {
-	function manageQuesBank() {
+	function bulkUpload() {
+		try {
+			echo "manage";
+				
+		} catch (Exception $e) {
+			$this->handleException($e->getMessage());
+		}
+	}
+
+	function singleUpload() {
 		try {
 			echo "manage";
 				
---
+++ model/questionBankModel.php
@@ -0,0 +1,14 @@
+<?php
+include MODEL_PATH."db_connect.php";
+class questionbankModel extends dbConnectModel {
+	function __construct() {
+		parent::__construct();
+	}
+
+	function bulkUploadModel(){
+	}
+
+	function singleUploadModel(){
+	}
+}
+?>
