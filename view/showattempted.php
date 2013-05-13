12a5f3dda681ac6c7eab6ac416bb5e0d0e91dda7
rawatsun <suraj.singh@osscube.com>
2013-05-13 05:55:59 -0700

    show attempted

--- view/showattempted.php
+++ view/showattempted.php
@@ -1,11 +1,13 @@
-	
+
+  <div class="contact-strip bg-mid-gray">Your Attempted / unAttempted Questions</div>
+  	<div class="space"></div>
 <?php
 
 if(!empty($_SESSION['answers'])) {
 	foreach ($arrData as $key => $value) {
-		echo "<a href='http://test_scheduler.com".$_SERVER['REQUEST_URI']."?id=1'>";   
-		echo $arrData["$key"]['question']."</a>"."<br/>";
-		echo $arrData["$key"]['option']."<br/>"."<br/>";
+		echo "<a href='#' class=submmit_button_generic>";   
+		echo $arrData["$key"]['question']."</a>"."<div class=space></div><div class=space></div>";
+		echo "<label>".$arrData["$key"]['option']."</label>"."<br/>"."<div class=space></div><div class=space></div>";
 	}
 	} else {
 	echo "NO Question Answered";
