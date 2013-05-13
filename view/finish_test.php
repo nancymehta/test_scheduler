f9ab4cc1e0e96ed4dbfc8b82df4e3326f303620b
rawatsun <suraj.singh@osscube.com>
2013-05-13 01:37:26 -0700

    test_exit styled

--- view/finish_test.php
+++ view/finish_test.php
@@ -1,14 +1,29 @@
-<br/><br/><br/><br/><br/><br/>
-"The TEst is finished NOw "<br/>
+<div class="space"></div>
+<div class="space"></div>
+
+<div class="bigmid" >
+<div class="midpanel" >
+
+<!-- midpanel Content gooes here  -->
+
+
+
+
+  <div class="contact-strip bg-mid-gray">
+	The Test is finished NOw ..
+
+  	</div>
+  	<div class="space"></div>
+
 <?php 
 	
 //print_r($arrData); 
 //print_r($_SESSION); 
 if(!empty($arrData) && isset($_SESSION['guest_id'])) {
-	echo "Name : ".$arrData['first_name']." ".$arrData['last_name']."<br/>";
-	echo "Email/Enroll No : ".$arrData['email_enroll_no']." <br/>";
+	echo "Name : ".$arrData['first_name']." ".$arrData['last_name']."<br/><br/>";
+	echo "Email/Enroll No : ".$arrData['email_enroll_no']." <br/><br/>";
 
-	echo " Correct Answer : ". $arrData['score']." <br/>";
+	echo " Correct Answer : ". $arrData['score']." <br/><br/>";
 	$percentage="0";
 	if($arrData['score']!=0 && $arrData['score']!="" ) {
 		$percentage=(($arrData['score']/$arrData['total_ques'])*100);
@@ -29,12 +44,24 @@ if(!empty($arrData) && isset($_SESSION['guest_id'])) {
 
 
 ?>
+<div class="space"></div>
 <form action="#" method="post">
-<textarea name="feedback"> </textarea>
+
+<textarea name="feedback" class="textarea"> </textarea>
+
+<div class="space"></div>
 	<input type="hidden" name="controller" value="test" >	
-	<input type="submit" name='function' value="feedback">
+	<input type="submit" name='function' value="feedback" class="submmit_button_generic">
 </form>
 <form action="#" method="post">
 	<input type="hidden" name="controller" value="test" >
-	<input type="submit" name='function' value="exitTest">
+	<input type="submit" name='function' value="exitTest" class="submmit_button_generic">
 </form>
+</div>
+<div class="midright">
+
+<!-- right content goes here -->
+</div>
+
+</div>
+
