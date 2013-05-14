68da915613ddf44e14c8cf2d81d60e4d1e6768f1
bhanu007 <bhanu.gill@osscube.com>
2013-05-13 21:30:56 -0700

    view category by question is added

---
+++ view/user_examiner_view/view_category_by_question.php
@@ -0,0 +1,63 @@
+<div class="bigmid" >
+<div class="midpanel" >
+
+<!-- midpanel Content gooes here  -->
+
+<div class="para"></div>
+<div class = "category_content">
+
+	
+	
+
+<div class="fancybox">
+	<div class="box">
+	<form action="jasd">
+
+	
+		Search Topic:<input type ="text" name="cat_name"><br><br><br>
+	
+	</form>
+	</div>
+</div>
+
+
+
+
+<div class="space"></div>
+	<div class = "category_div">
+<!-- abhishek -->
+	<form id="addCategoryForm"  name="addCategoryForm" method="post">
+
+		
+		<div class="space"></div>
+		
+	</form> 
+	</div> 
+	<div class = "catergory_list">
+		<table class = "table-generic table1">
+			
+			 <thead><tr>
+				<th>S.NO</th>
+				<th>Category</th>
+							
+			<tr>
+				<td> sno</td>
+				<td><a href="">name</a></td>
+				<!--<td><a href="#" onclick="$('.b1').click()">Edit</a></td>-->
+				
+			</tr>
+			
+			</tbody>
+		</table>
+	</div>
+</div>
+
+</div>
+<div class="midright">
+
+<!-- right content goes here -->
+</div> 
+
+</div>
+
+
