bbec168c14c5e75ff8e86322d51ee6b91c30828c
bhanu007 <bhanu.gill@osscube.com>
2013-05-17 23:12:16 -0700

    css change

--- view/user_examiner_view/deshboard_menu.php
+++ view/user_examiner_view/deshboard_menu.php
@@ -1,6 +1,27 @@
 <div class="menu_container bg-mid-gray">
 
+<div class="links">
+					<a id = "home" href ="<?php echo SITE_PATH; ?>">Home »</a>
+					<a id = "assign" class="services" href = "#">Assign »</a>
+					<a id = "test" class="tech" href = "<?php echo SITE_PATH."allTest"; ?>">Test »</a>
+					<a id = "result" href = '<?php echo SITE_PATH."/result/result";?>'>Results »</a>
+					<a id = "test" class="tech" href = '<?php echo SITE_PATH."/faq";?>'>Faq's »</a>
+					<a id = "test" class="tech" href = "#">Test »</a>
+					<a id = "test" class="tech" href = '<?php echo SITE_PATH."settings";?>'>Account Settings »</a>
+				</div>	
+				<div class="menu-services ">
+						<ul>
+			<li><a href = "<?php echo SITE_PATH."mytest"; ?>">My Tests</a></li>
+			<li><a href = "<?php echo SITE_PATH."questionbank"; ?>">Question Bank</a></li>
+			<li><a href = "<?php echo SITE_PATH."category"; ?>">categories</a></li>
+			<li><a href = "<?php echo SITE_PATH."certificate"; ?>">certificates</a></li>
+					</ul>
+					</div>			
+					
 
+
+
+</div>
 
 
 <div class="space"></div>
