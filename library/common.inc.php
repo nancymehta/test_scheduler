c4c96cadac75e3a7ea9962f67960103c41b280f0
rahulramachandran <rahul00015@gmail.com>
2013-05-19 12:39:11 -0700

    email function modified common.inc.php

--- library/common.inc.php
+++ library/common.inc.php
@@ -45,53 +45,107 @@ function myErrorHandler($errno, $errstr, $errfile, $errline) {
     /* Don't execute PHP internal error handler */
     return true;
 }
+	
 	/*
+	 * function emailBody()
+	 * emailBody constructs the email body and calls the mailing function
+	 * to send email to intended recipient(s).
+	 *
+	 * KINDLY GO THROUGH THE FOLLOWING COMMENT BEFORE USING THIS FUNCTION.
+	 *
+	 * USE THE INDEX NAME EXACTLY AS MENTIONED.
+	 * AND USE ALL THE MENTIONED INDEXES CORRESPONDING TO A GIVEN email_type.
 	 * 
+	 * TO USE THIS FUNCTION CONSTRUCT THE FOLLOWING ARRAY.
+	 * 
+	 * $arrArgs=array(
+		"email_type" => "registration  	OR 	feedback	OR 		contactus		OR 		link",
+		"to" => "SENDERS ADDRESS HERE (COMMA SEPARATED RECIPIENTS LIST)",
+		"subject" => "EMAIL SUBJECT HERE",
+	 );
+	 *
+	 * If email_type is either of registration  	OR 	feedback	OR 		contactus ,
+	 * then add the following details to the $arrArgs
+	 * $arrArgs=array(
+		"id" => "ID OF THE USER"
+	 );
+	 *
+	 * For registration email_type, a password field can also be added if possible.
+	 * $arrArgs=array(
+		"password" => "PASSWORD USED FOR REGISTRATION"
+	 );
+	 *
+	 * If email_type is link , add the following indexes to the $arrArgs array
+	 * $arrArgs=array(
+		"link" => "LINK ADDRESS GOES HERE" ,
+		"test_name" => "TEST NAME GOES HERE" ,
+		"start_time" => "START TIME FOR THE TEST GOES HERE."
+	 );
 	 * */
-	function createEmailTemplate($arrData=array()) {
+	function emailBody($arrData=array()) {
 		if($arrData) {
-			$emailBody="";
-			/*
-			 * Fetching User Name
-			 * */
-			$data=array();
-			$data['tables'] = 'user_profile';
-			$data['columns']= array('first_name','last_name');
-			$data['conditions']	= array(
-					"user_id" => $arrData['id'],
-			);
-			$result = $this->_db->select($data);
-			$row = $result->fetch(PDO::FETCH_ASSOC);
-			$userName=$row['first_name']. " " . $row['last_name'];
-			
-			/*
-			 * Fetching Email Template Content
-			 * */
-			$data=array();
-			$data['tables'] = 'email_template';
-			$data['columns']= array('email_content');
-			$data['conditions']	= array(
-					"email_type" => $arrData['email_type'],
-					"status" => "0"
-			);
-			$result = $this->_db->select($data);
-			
-			$row = $result->fetch(PDO::FETCH_ASSOC);
-			$emailContent=$row['email_content'];
-			$emailBody=str_replace("<USER>",$userName,$emailContent);
-			
-			/*
-			 * Use the following code in case more parameters are needed to be replaced in the
-			 * email content.
-			 * */
-/* 			if($arrData['type']=="registration") {
+			if($arrData['email_type'] && $arrData['to'] && $arrData['subject']) {
+				$template_type=array(
+					"registration",
+					"feedback",
+					"contactus",
+					"link"
+				);
 				
-			} else  if($arrData['type']=="feedback") {
-				
-			} else  if($arrData['type']=="contactus") {
-				
-			} */
-			return $emailBody; 
+				if(in_array($arrData['email_type'],$template_type)) {
+					/*
+					 * Fetching Email Template Content
+					 * */
+					$data=array();
+					$data['tables'] = 'email_template';
+					$data['columns']= array('email_content');
+					$data['conditions']	= array(
+							"email_type" => $arrData['email_type'],
+							"status" => "0"
+					);
+					$result = $this->_db->select($data);
+					$row = $result->fetch(PDO::FETCH_ASSOC);
+					$emailContent=$row['email_content'];
+					
+					if($arrData['email_type']=="link") {
+						$emailContent=str_replace("<TEST_NAME>",$arrData['test_name'],$emailContent);
+						$emailContent=str_replace("<START_TIME>",$arrData['start_time'],$emailContent);
+						$emailContent=str_replace("<LINK>",$arrData['link'],$emailContent);
+					} else {
+						/*
+						* Fetching User Name
+						* */
+						if($arrData['id']) {
+							$data=array();
+							$data['tables'] = 'user_profile';
+							$data['columns']= array('first_name','last_name');
+							$data['conditions']	= array(
+								   "user_id" => $arrData['id'],
+							);
+							$result = $this->_db->select($data);
+							$row = $result->fetch(PDO::FETCH_ASSOC);
+							$userName=$row['first_name']. " " . $row['last_name'];
+							$emailContent=str_replace("<USER>",$userName,$emailContent);
+							if($arrData['email_type']=="registration") {
+								if($arrData['password']) {
+									$emailContent=str_replace("<PASSWORD>",$arrData['password'],$emailContent);
+								}
+							}
+						} else {
+							$_SESSION['SESS_ERROR']="No User Provided For Email.<br/>";
+							exit;
+						}
+					}
+					mailTest($arrData['to'],$arrData['subject'],$emailContent,$arrData['attach']);
+				}
+				else {
+					$_SESSION['SESS_ERROR']="Email Type Not Recognised.<br/>";
+					exit;
+				}
+			} else {
+				$_SESSION['SESS_ERROR']="Insufficient Parameters Passed For Mail<br/>";
+				exit;
+			}
 		} else {
 			$_SESSION['SESS_ERROR']="No Data Recieved For Email Template.<br/>";
 		}
@@ -107,19 +161,24 @@ function mailTest($to,$sub,$body,$attach='') {
 	$mail->Password = "osscube@123"; // SMTP password
 	$mail->SMTPSecure = 'tls';
 	$mail->From = "info.test.scheduler@gmail.com"; 
-	$mail->FromName = "Admin_Test_Scheduler"; 
-	$mail->AddAddress("$to","Member"); 
+	$mail->FromName = "Admin_Test_Scheduler";
+	$temp=explode(",",$to);
+	$counter=1;
+	foreach($temp as $address) {
+		$mail->AddAddress("$address","Member".$counter);
+		$counter++;
+	}
 	$mail->WordWrap = 50; // set word wrap 
 	$mail->IsHTML(true); 
 	$mail->Subject = "$sub"; 
 	$mail->Body = "$body"; 
 	$mail->AltBody = "Mail Form test Scheduler";
 	$mail->AddAttachment($attach);
-	if(!$mail->Send()) 
-	{ 
-	echo "Message was not sent <p>"; 
-	echo "Mailer Error: " . $mail->ErrorInfo; 
-	exit; 	}	
+	if(!$mail->Send()) { 
+		echo "Message was not sent <p>"; 
+		echo "Mailer Error: " . $mail->ErrorInfo; 
+		exit;
+	}	
 
 echo "Message has been sent"; 
 }
