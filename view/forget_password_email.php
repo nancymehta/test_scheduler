aada6d6ceefb1500dfbfa520c221b6db924f8e02
shashank13verma <shashank.verma@osscube.com>
2013-05-24 20:57:47 -0700

    forgot password

---
+++ view/forget_password_email.php
@@ -0,0 +1,9 @@
+<div class = "mainContainer">
+	<div class="bigmid" >
+
+We have sent the password reset link to your  email id <b><?php echo $_SESSION['email']; ?></b>
+<div class="space"></div>
+<input type="submit" class="button_generic floatl" value="Request Password" name="req_password" id="req_password">
+</form>
+</div>
+</div>
---
+++ view/reset_pass.php
@@ -0,0 +1,8 @@
+<div class = "mainContainer">
+	<div class="bigmid" >
+<form method="post" action="<?php echo SITE_PATH.'main/passChanged';?>">
+enter your new password:<input type="password" name="password" /><div class="space"></div>
+<input type="submit" value="Change Password">
+</form>
+</div>
+</div>
